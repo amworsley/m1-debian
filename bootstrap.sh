06535e0d0eccdfde09248758f6e2007eb660e9a4
Thomas Glanzmann <thomas@glanzmann.de>
2022-01-22 21:29:59 +0100

    m1 config options

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,12 @@ cat m1n1/build/m1n1.macho `find u-boot -name \*.dtb` u-boot/u-boot-nodtb.bin > u
 git clone https://github.com/AsahiLinux/linux
 cd linux
 git checkout origin/asahi
-curl https://tg.st/u/c5eb67144c10f8685ebd8c1dfef8586588e1994d.patch | patch -p1
-curl https://tg.st/u/asahi-config-2022-01-08 > .config
+# SPI patches
+curl https://tg.st/u/aa6a11b3feeda0f57284f99406188e4615e7c43c.patch | patch -p1
+curl https://tg.st/u/9ce9060dea91951a330feeeda3ad636bc88c642c.patch | patch -p1
+# Sound patch
+curl https://tg.st/u/5nly | patch -p1
+# Config with sound enabled
+curl https://tg.st/u/asahi-config-2022-01-19 > .config
 make olddefconfig
 make bindeb-pkg
