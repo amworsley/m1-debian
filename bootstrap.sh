5920ad39aca05765cd860523d6784286568877df
Thomas Glanzmann <thomas@glanzmann.de>
2022-01-25 11:32:13 +0100

    work in progress

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,34 +6,47 @@ set -e
 unset LC_CTYPE
 unset LANG
 
-# Build u-boot
-git clone https://github.com/kettenis/u-boot
-cd u-boot/
-git checkout apple-m1-m1n1-nvme
-make apple_m1_defconfig
-# it is normal that it runs on an error at the end
-make || true
-cd ..
+build_m1n1()
+{
+(
+        test -d m1n1 || git clone --recursive https://github.com/AsahiLinux/m1n1.git
+        cd m1n1
+        git fetch
+        git reset --hard origin/master; git clean -f -x -d
+        make -j
+)
+}
 
-# build m1n1
-git clone --recursive https://github.com/AsahiLinux/m1n1.git
-cd m1n1
-make -j
-cd ..
+build_uboot()
+{
+(
+        # Build u-boot
+        test -d u-boot | git clone https://github.com/kettenis/u-boot
+        cd u-boot
+        git reset --hard origin/apple-m1-m1n1-nvme; git clean -f -x -d
+        make apple_m1_defconfig
+        # it is normal that it runs on an error at the end
+        make || true
+)
 
-# Build our boot object that replaces step2.sh in the asahi installer
-cat m1n1/build/m1n1.macho `find u-boot -name \*.dtb` u-boot/u-boot-nodtb.bin > u-boot.macho
+        cat m1n1/build/m1n1.macho `find u-boot -name \*.dtb` u-boot/u-boot-nodtb.bin > u-boot.macho
+}
 
-# Build Linux
-git clone https://github.com/AsahiLinux/linux
-cd linux
-git checkout origin/asahi
-# SPI patches
-curl https://tg.st/u/aa6a11b3feeda0f57284f99406188e4615e7c43c.patch | patch -p1
-curl https://tg.st/u/9ce9060dea91951a330feeeda3ad636bc88c642c.patch | patch -p1
-# Sound patch
-curl https://tg.st/u/5nly | patch -p1
-# Config with sound enabled
-curl https://tg.st/u/asahi-config-2022-01-19 > .config
-make olddefconfig
-make bindeb-pkg
+
+# # Build our boot object that replaces step2.sh in the asahi installer
+# 
+# # Build Linux
+# git clone https://github.com/AsahiLinux/linux
+# cd linux
+# git checkout origin/asahi
+# # SPI patches
+# curl https://tg.st/u/aa6a11b3feeda0f57284f99406188e4615e7c43c.patch | patch -p1
+# curl https://tg.st/u/9ce9060dea91951a330feeeda3ad636bc88c642c.patch | patch -p1
+# # Sound patch
+# curl https://tg.st/u/5nly | patch -p1
+# # Config with sound enabled
+# curl https://tg.st/u/asahi-config-2022-01-19 > .config
+# make olddefconfig
+# make bindeb-pkg
+
+build_m1n1
