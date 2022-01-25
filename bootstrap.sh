ce0c2c8bbfaa9be92b1cc74d04673eebe5562433
Thomas Glanzmann <thomas@glanzmann.de>
2022-01-25 11:33:48 +0100

    working on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ build_m1n1()
         test -d m1n1 || git clone --recursive https://github.com/AsahiLinux/m1n1.git
         cd m1n1
         git fetch
-        git reset --hard origin/master; git clean -f -x -d
+        git reset --hard origin/main; git clean -f -x -d
         make -j
 )
 }
@@ -21,12 +21,12 @@ build_uboot()
 {
 (
         # Build u-boot
-        test -d u-boot | git clone https://github.com/kettenis/u-boot
+        test -d u-boot || git clone https://github.com/kettenis/u-boot
         cd u-boot
         git reset --hard origin/apple-m1-m1n1-nvme; git clean -f -x -d
         make apple_m1_defconfig
         # it is normal that it runs on an error at the end
-        make || true
+        make -j 16 || true
 )
 
         cat m1n1/build/m1n1.macho `find u-boot -name \*.dtb` u-boot/u-boot-nodtb.bin > u-boot.macho
@@ -49,4 +49,5 @@ build_uboot()
 # make olddefconfig
 # make bindeb-pkg
 
-build_m1n1
+# build_m1n1
+build_uboot
