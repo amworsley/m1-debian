d0071c0bc7b0d90738ac2ef3275cce2cc69b5108
Thomas Glanzmann <thomas@glanzmann.de>
2022-01-25 11:52:59 +0100

    build linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,7 @@ build_m1n1()
         cd m1n1
         git fetch
         git reset --hard origin/main; git clean -f -x -d
-        make -j
+        make -j 16
 )
 }
 
@@ -23,6 +23,7 @@ build_uboot()
         # Build u-boot
         test -d u-boot || git clone https://github.com/kettenis/u-boot
         cd u-boot
+        git fetch
         git reset --hard origin/apple-m1-m1n1-nvme; git clean -f -x -d
         make apple_m1_defconfig
         # it is normal that it runs on an error at the end
@@ -32,22 +33,23 @@ build_uboot()
         cat m1n1/build/m1n1.macho `find u-boot -name \*.dtb` u-boot/u-boot-nodtb.bin > u-boot.macho
 }
 
+build_linux()
+{
+(
+        test -d linux || git clone --depth 1 https://github.com/AsahiLinux/linux
+        cd linux
+        git fetch
+        git reset --hard origin/asahi; git clean -f -x -d
+        curl -s https://tg.st/u/9ce9060dea91951a330feeeda3ad636bc88c642c.patch | git am -
+        curl -s https://tg.st/u/5nly | git am -
+        curl -s https://tg.st/u/asahi-config-2022-01-19 > .config
+        make olddefconfig
+        make bindeb-pkg
+)
+}
 
-# # Build our boot object that replaces step2.sh in the asahi installer
-# 
-# # Build Linux
-# git clone https://github.com/AsahiLinux/linux
-# cd linux
-# git checkout origin/asahi
-# # SPI patches
-# curl https://tg.st/u/aa6a11b3feeda0f57284f99406188e4615e7c43c.patch | patch -p1
-# curl https://tg.st/u/9ce9060dea91951a330feeeda3ad636bc88c642c.patch | patch -p1
-# # Sound patch
-# curl https://tg.st/u/5nly | patch -p1
-# # Config with sound enabled
-# curl https://tg.st/u/asahi-config-2022-01-19 > .config
-# make olddefconfig
-# make bindeb-pkg
+
 
 # build_m1n1
-build_uboot
+# build_uboot
+build_linux
