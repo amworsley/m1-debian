dc18c65cb45a9fe4c724ccf953060fbd38507be9
Thomas Glanzmann <thomas@glanzmann.de>
2022-02-13 22:32:10 +0100

    install grub

--- files/boot.sh
+++ files/boot.sh
@@ -3,6 +3,7 @@
 echo 'grub-efi-arm64 grub2/update_nvram boolean false' | chroot /target debconf-set-selections
 echo 'grub-efi-arm64 grub2/force_efi_extra_removable boolean false' | chroot /target debconf-set-selections
 chroot /target apt-get remove grub-efi-arm64-signed
+chroot /target grub-install --removable /boot/efi
 rm /target/boot/efi/EFI/BOOT/fbaa64.efi
 rm /target/boot/efi/EFI/debian/fbaa64.efi
 cp -a /lib/firmware/brcm /target/lib/firmware/
