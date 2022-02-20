f1f2b82f8214511b6de0d8b38e10145ce85e69c1
Thomas Glanzmann <thomas@glanzmann.de>
2022-02-20 07:46:31 +0100

    drop the 4k patch for now

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,7 +27,7 @@ build_linux()
         cd linux
         git fetch
         git reset --hard origin/asahi; git clean -f -x -d &> /dev/null
-        curl -s https://tg.st/u/0001-4k-iommu-patch.patch | git am -
+        # curl -s https://tg.st/u/0001-4k-iommu-patch.patch | git am -
         curl -s https://tg.st/u/DXyl.patch | git am -
         curl -s https://tg.st/u/0001-apple-mca-correct-prinkts.patch | git am -
         curl -s https://tg.st/u/config-2022-02-19 > .config
