6620d516ef11236962275b1c43c26f21b52bbbfb
Thomas Glanzmann <thomas@glanzmann.de>
2022-03-11 08:47:25 +0100

    more notes

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,6 +28,7 @@ build_linux()
         git fetch
         git reset --hard origin/asahi; git clean -f -x -d &> /dev/null
         curl -s https://tg.st/u/0001-4k-iommu-patch.patch | git am -
+        curl -s https://tg.st/u/40c9642c7569c52189f84621316fc9149979ee65.patch | git am -
         curl -s https://tg.st/u/config-2022-03-09-4k > .config
         make olddefconfig
         make -j `nproc` V=0 bindeb-pkg > /dev/null
