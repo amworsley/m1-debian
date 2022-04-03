2dda0f6f3611eac90358f7eeec4e1803f9cacc51
Andrew Worsley <amworsley@gmail.com>
2022-04-03 10:24:12 +1000

    Add support -n - print commands instead of running

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,14 @@ export DEBOOTSTRAP=debootstrap
 
 usage()
 {
-   echo "$(basename $0): Build binares to install M1 linux"
-   echo ""
-   echo "-h : Print usage information"
-   #echo "-n : Dry-run print commands instead of executing them"
-   echo "-x : Enabling tracing of shell script"
-   echo "-q : Disable tracing of shell script"
+   echo "Build binaries to install Asahi M1 linux"
+   echo "Usage:"
+   echo " $(basename $0) [Options] [<Command> ...]"
+   echo "Options:"
+   echo " -h : Print usage information"
+   echo " -n : Dry-run print commands instead of executing them"
+   echo " -x : Enabling tracing of shell script"
+   echo " -q : Disable tracing of shell script"
    echo
    echo "Commands: (defaults to doing all)"
    echo " install : Install require debian packages"
@@ -50,6 +52,7 @@ build_linux()
 {
 (
         handle_crosscompile
+$DO_CMD <<EOF
         test -d linux || git clone --depth 1 https://github.com/AsahiLinux/linux -b asahi
         cd linux
         git fetch
@@ -59,17 +62,20 @@ build_linux()
         curl -s https://tg.st/u/config-2022-03-17-distro-sven-jannau.txt > .config
         make olddefconfig
         make -j `nproc` V=0 bindeb-pkg > /dev/null
+EOF
 )
 }
 
 build_m1n1()
 {
 (
+$DO_CMD <<EOF
         test -d m1n1 || git clone --recursive https://github.com/AsahiLinux/m1n1
         cd m1n1
         git fetch
         git reset --hard origin/main; git clean -f -x -d &> /dev/null
         make -j `nproc`
+EOF
 )
 }
 
@@ -77,14 +83,17 @@ build_uboot()
 {
 (
         handle_crosscompile
+$DO_CMD <<EOF
         test -d u-boot || git clone --depth 1 https://github.com/AsahiLinux/u-boot
         cd u-boot
         git fetch
         git reset --hard origin/asahi; git clean -f -x -d &> /dev/null
         make apple_m1_defconfig
         make -j `nproc`
+EOF
 )
 
+$DO_CMD <<EOF
         cat m1n1/build/m1n1.bin   `find linux/arch/arm64/boot/dts/apple/ -name \*.dtb` <(gzip -c u-boot/u-boot-nodtb.bin) > u-boot.bin
         cat m1n1/build/m1n1.macho `find linux/arch/arm64/boot/dts/apple/ -name \*.dtb` <(gzip -c u-boot/u-boot-nodtb.bin) > u-boot.macho
         cp u-boot.bin 4k.bin
@@ -92,12 +101,14 @@ build_uboot()
         echo 'display=2560x1440' >> 2k.bin
         echo 'display=wait,3840x2160' >> 4k.bin
 
+EOF
 }
 
 build_rootfs()
 {
 (
         handle_crosscompile
+$DO_CMD <<EOF
         sudo rm -rf testing
         mkdir -p cache
         sudo eatmydata ${DEBOOTSTRAP} --cache-dir=`pwd`/cache --arch=arm64 --include initramfs-tools,pciutils,wpasupplicant,tcpdump,vim,tmux,vlan,ntpdate,parted,curl,wget,grub-efi-arm64,mtr-tiny,dbus,ca-certificates,sudo,openssh-client,mtools,gdisk testing testing http://deb.debian.org/debian
@@ -132,12 +143,14 @@ build_rootfs()
         sudo rm ${KERNEL}
 
         sudo bash -c 'chroot . apt-get clean'
+EOF
 )
 }
 
 build_live_stick()
 {
 (
+$DO_CMD <<EOF
         rm -rf live-stick
         mkdir -p live-stick/efi/boot live-stick/efi/debian/
         sudo cp ../files/wifi.pl testing/etc/rc.local
@@ -146,12 +159,14 @@ build_live_stick()
         cp testing/boot/vmlinuz* live-stick/vmlinuz
         cp ../files/grub.cfg live-stick/efi/debian/grub.cfg
         (cd live-stick; tar cf ../asahi-debian-live.tar .)
+EOF
 )
 }
 
 build_dd()
 {
 (
+$DO_CMD <<EOF
         rm -f media
         dd if=/dev/zero of=media bs=1 count=0 seek=2G
         mkdir -p mnt
@@ -162,12 +177,14 @@ build_dd()
         sudo rm mnt/init
         sudo umount mnt
         tar cf - media | pigz -9 > m1.tgz
+EOF
 )
 }
 
 build_efi()
 {
 (
+$DO_CMD <<EOFXX
         rm -rf EFI
         mkdir -p EFI/boot EFI/debian
         cp testing/usr/lib/grub/arm64-efi/monolithic/grubaa64.efi EFI/boot/bootaa64.efi
@@ -182,12 +199,14 @@ initrd (\$root)/boot/${INITRD}
 boot
 EOF
         tar czf efi.tgz EFI
+EOFXX
 )
 }
 
 build_asahi_installer_image()
 {
 (
+$DO_CMD <<EOF
         rm -rf aii
         mkdir -p aii/esp/m1n1
         cp -a EFI aii/esp/
@@ -195,11 +214,13 @@ build_asahi_installer_image()
         ln media aii/media
         cd aii
         zip -r9 ../debian-base.zip esp media
+EOF
 )
 }
 
 build_di_stick()
 {
+$DO_CMD <<EOF
         rm -rf di-stick
         mkdir -p di-stick/efi/boot di-stick/efi/debian/
         rm -f initrd.gz
@@ -217,15 +238,19 @@ build_di_stick()
         export KERNEL=`ls -1rt linux-image*.deb | grep -v dbg | tail -1`
         cp ${KERNEL} di-stick/
         (cd di-stick; tar cf ../m1-d-i.tar .)
+EOF
 }
 
 publish_artefacts()
 {
         export KERNEL=`ls -1rt linux-image*.deb | grep -v dbg | tail -1`
+$DO_CMD <<EOF
         cp ${KERNEL} k.deb
         sudo cp m1-d-i.tar m1.tgz efi.tgz asahi-debian-live.tar u-boot.bin u-boot.macho 2k.bin 4k.bin k.deb m1n1/build/m1n1.bin m1n1/build/m1n1.macho testing/usr/lib/grub/arm64-efi/monolithic/grubaa64.efi debian-base.zip /u/
+EOF
 }
 
+CMD="/bin/bash"
 while getopts 'hnxqC:' argv
 do
     case $argv in
@@ -240,16 +265,23 @@ do
     q)
         echo "Disable tracing"
         set +x
+	CMD="/bin/bash"
     ;;
     x)
         echo "Enabling tracing"
         set -x
+	CMD="/bin/bash -x"
     ;;
     C)
        CONF="$OPTARG"
     ;;
     esac
 done
+if [ -n "$DR" ]; then
+    DO_CMD="cat"
+else
+    DO_CMD="$CMD"
+fi
 
 $DR mkdir -p build
 $DR cd build
