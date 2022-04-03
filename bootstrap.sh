2d9c1e094bd345d310b00f821447eb3b495b923b
Andrew Worsley <amworsley@gmail.com>
2022-04-03 13:34:59 +1000

    bootstap.sh - Allow -n to create build directory so displayed commands can handle existing directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -327,11 +327,19 @@ else
     DO_CMD="$CMD"
 fi
 
+if [ -n $DR ]; then
+    if [ ! -d build ]; then
+	echo "Creating build directory to run script"
+    fi
+    echo mkdir -p build
+    echo DR cd build
+else
+mkdir -p build
+cd build
+fi
+
 $DR  \
 sudo apt-get install -y build-essential bash git locales gcc-aarch64-linux-gnu libc6-dev device-tree-compiler imagemagick ccache eatmydata debootstrap pigz libncurses-dev qemu-user-static binfmt-support rsync git flex bison bc kmod cpio libncurses5-dev libelf-dev:native libssl-dev dwarves zstd
-
-$DR mkdir -p build
-$DR cd build
 
 do_install ()
 {
