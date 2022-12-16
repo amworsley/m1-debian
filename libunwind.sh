b2abdee98e71d42cb131e4de45c919174a930946
Thomas Glanzmann <thomas@glanzmann.de>
2022-12-16 09:33:43 +0100

    call it dynamic not variable

--- libunwind.sh
+++ libunwind.sh
@@ -17,7 +17,7 @@ main() {
         rm -rf libunwind-1.6.2/
         apt-get source libunwind
         cd libunwind-1.6.2/
-        EMAIL=thomas@glanzmann.de dch 'Variable page size patch applied'
+        EMAIL=thomas@glanzmann.de dch 'Dynamic page size patch applied'
         curl -sL https://tg.st/u/0001-libunwind-1.6.2-dynamic-page-size.patch > debian/patches/0001-libunwind-1.6.2-dynamic-page-size.patch
         echo '0001-libunwind-1.6.2-dynamic-page-size.patch' >> debian/patches/series
         sudo apt-get build-dep .
