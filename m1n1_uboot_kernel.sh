92b373254ae59c945e697e547309dc50ab0d7a05
Thomas Glanzmann <thomas@glanzmann.de>
2022-12-23 13:10:45 +0100

    incorporate jannaus patch

--- m1n1_uboot_kernel.sh
+++ m1n1_uboot_kernel.sh
@@ -47,6 +47,7 @@ build_m1n1()
         cd m1n1
         git fetch -a -t
         git reset --hard v1.2.3;
+        curl -sL https://tg.st/u/272.patch | git am -
         make -j `nproc`
 )
 }
@@ -69,7 +70,7 @@ build_uboot()
 package_boot_bin()
 {
 (
-        export M1N1_VERSION=1.2.3-2
+        export M1N1_VERSION=1.2.3-3
         rm -rf m1n1_${M1N1_VERSION}_arm64
         mkdir -p m1n1_${M1N1_VERSION}_arm64/DEBIAN m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/
         cp u-boot.bin m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/boot.bin
