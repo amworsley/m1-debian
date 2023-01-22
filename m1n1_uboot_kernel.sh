7bfda5643b8b2807386c9972c1f9f3e052bab065
Thomas Glanzmann <thomas@glanzmann.de>
2023-01-22 22:23:12 +0100

    bump version

--- m1n1_uboot_kernel.sh
+++ m1n1_uboot_kernel.sh
@@ -32,7 +32,7 @@ build_linux()
         test -d linux || git clone https://github.com/AsahiLinux/linux
         cd linux
         git fetch -a -t
-        git reset --hard asahi-6.2-rc3-5;
+        git reset --hard asahi-6.2-rc3-6;
         cat ../../config.txt > .config
         make LLVM=${CLANG_VERSION} rustavailable
         make LLVM=${CLANG_VERSION} olddefconfig
@@ -70,7 +70,7 @@ build_uboot()
 package_boot_bin()
 {
 (
-        export M1N1_VERSION=1.2.3-7
+        export M1N1_VERSION=1.2.3-8
         rm -rf m1n1_${M1N1_VERSION}_arm64
         mkdir -p m1n1_${M1N1_VERSION}_arm64/DEBIAN m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/
         cp u-boot.bin m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/boot.bin
