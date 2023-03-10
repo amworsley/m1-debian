1c1106d1564aeddef6820b1a98353a05e762b3e2
Thomas Glanzmann <thomas@glanzmann.de>
2023-03-10 22:45:03 +0100

    +=Provides: wireguard-modules (= 1.0.0)
    * Jefferson Noxon <jeff@noxon.cc> [2023-03-10 22:00]:
    I noticed that when I try to install wireguard-tools, it wants to pull in a
    different kernel package, linux-image-rt-arm64. This is a meta package with
    
    Provides: wireguard-modules (=1.0.0).
    
    It would be awesome if linux-image-asahi also had this Provides, as
    linux-image-6.1.0-asahi does contain wireguard.ko.

--- meta.sh
+++ meta.sh
@@ -10,24 +10,20 @@ cd "$(dirname "$0")"
 unset LC_CTYPE
 unset LANG
 
-main() {
-        mkdir -p build
-        cd build
-        export META_VERSION=6.2.0-1
-        rm -rf linux-image-asahi_${META_VERSION}_arm64
-        mkdir -p linux-image-asahi_${META_VERSION}_arm64/DEBIAN
+mkdir -p build
+cd build
+export META_VERSION=6.1.0-3
+rm -rf linux-image-asahi_${META_VERSION}_arm64
+mkdir -p linux-image-asahi_${META_VERSION}_arm64/DEBIAN
 cat > linux-image-asahi_${META_VERSION}_arm64/DEBIAN/control <<EOF
 Package: linux-image-asahi
 Version: $META_VERSION
 Section: base
-Depends: linux-image-6.2.0-rc2-asahi (>= 6.2.0-rc2-asahi-62)
+Depends: linux-image-6.1.0-asahi (>= 6.1.0-asahi-61)
+Provides: wireguard-modules (= 1.0.0)
 Priority: optional
 Architecture: arm64
 Maintainer: Thomas Glanzmann <thomas@glanzmann.de>
 Description: Linux for 64-bit apple silicon machines (meta-package)
 EOF
-        dpkg-deb --build linux-image-asahi_${META_VERSION}_arm64
-}
-
-main "$@"
-
+dpkg-deb --build linux-image-asahi_${META_VERSION}_arm64
