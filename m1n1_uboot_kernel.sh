72482bacb85b88171a404842177a214c0cdc5d95
Thomas Glanzmann <thomas@glanzmann.de>
2023-03-22 08:48:58 +0100

    bump m1n1 package version spotted by Andreas

--- m1n1_uboot_kernel.sh
+++ m1n1_uboot_kernel.sh
@@ -69,7 +69,7 @@ build_uboot()
 package_boot_bin()
 {
 (
-        export M1N1_VERSION=1.2.4-2
+        export M1N1_VERSION=1.2.6
         rm -rf m1n1_${M1N1_VERSION}_arm64
         mkdir -p m1n1_${M1N1_VERSION}_arm64/DEBIAN m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/
         cp u-boot.bin m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/boot.bin
