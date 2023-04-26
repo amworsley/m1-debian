d201f0293b9b3de381595079332d4f16e4831f6d
Thomas Glanzmann <thomas@glanzmann.de>
2023-04-26 08:58:15 +0200

    dont expand date in m1n1 postinst script

--- m1n1_uboot_kernel.sh
+++ m1n1_uboot_kernel.sh
@@ -32,7 +32,7 @@ build_linux()
         test -d linux || git clone https://github.com/AsahiLinux/linux
         cd linux
         git fetch -a -t
-        git reset --hard asahi-6.2-11;
+        git reset --hard asahi-6.2-12;
         cat ../../config.txt > .config
         make LLVM=${CLANG_VERSION} rustavailable
         make LLVM=${CLANG_VERSION} olddefconfig
@@ -69,7 +69,7 @@ build_uboot()
 package_boot_bin()
 {
 (
-        export M1N1_VERSION=1.2.6
+        export M1N1_VERSION=1.2.6-1
         rm -rf m1n1_${M1N1_VERSION}_arm64
         mkdir -p m1n1_${M1N1_VERSION}_arm64/DEBIAN m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/
         cp u-boot.bin m1n1_${M1N1_VERSION}_arm64/usr/lib/m1n1/boot.bin
@@ -84,7 +84,7 @@ Description: Apple silicon boot loader
  Next to m1n1 this also contains the device trees and u-boot.
 EOF
 
-        cat > m1n1_${M1N1_VERSION}_arm64/DEBIAN/postinst <<EOF
+        cat > m1n1_${M1N1_VERSION}_arm64/DEBIAN/postinst <<'EOF'
 #!/bin/bash
 
 export PATH=/bin
