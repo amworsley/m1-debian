3d73deff38205822d5a46f9608627f87e1656ae8
Thomas Glanzmann <thomas@glanzmann.de>
2023-04-27 22:58:39 +0200

    bump version

--- mesa.sh
+++ mesa.sh
@@ -20,7 +20,12 @@ command -v git >/dev/null || sudo apt-get install git
 test -d mesa || git clone https://gitlab.freedesktop.org/asahi/mesa.git
 cd mesa
 git fetch -a -t
-git reset --hard asahi-20230311
+# 17:17 <marcan> also for mesa, use the latest versioned tag I made, *not* any
+# live branch. mesa and kernel live branches are not kept in sync. generally
+# the latest mesa tag will be in sync with the latest kernel tag and usually
+# also the latest live kernel (except when I'm about to do a release)
+# 17:18 <marcan> if those two desync it'll refuse to initialize
+git reset --hard asahi-20230321
 rm -rf debian
 cp -a ../../mesa-debian debian
 EMAIL=thomas@glanzmann.de dch -v 23.0.0-`date +%Y%m%d%H%M` 'asahi wip'
