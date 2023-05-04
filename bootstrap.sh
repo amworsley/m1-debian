fc6942318aad43400a4e06b4bb8ec97218d75de4
Thomas Glanzmann <thomas@glanzmann.de>
2023-05-04 18:17:00 +0200

    patch does not apply

--- bootstrap.sh
+++ bootstrap.sh
@@ -127,8 +127,6 @@ publish_artefacts()
 mkdir -p build
 cd build
 
-sudo apt-get install -y build-essential bash git locales gcc-aarch64-linux-gnu libc6-dev device-tree-compiler imagemagick ccache eatmydata debootstrap pigz libncurses-dev qemu-user-static binfmt-support rsync git flex bison bc kmod cpio libncurses5-dev libelf-dev:native libssl-dev dwarves zstd
-
 build_rootfs
 build_dd
 build_efi
