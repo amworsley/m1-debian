1130b74c1be9ee690ea8f7e9a774f9225eb12d25
Andrew Worsley <amworsley@gmail.com>
2024-02-03 12:52:50 +1100

    add -h/n/q options to prepare_rust.sh

--- prepare_rust.sh
+++ prepare_rust.sh
@@ -5,11 +5,57 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-cd "$(dirname "$0")"
+usage()
+{
+   echo "Prepares a rust installation suitable for kernel compilation"
+   echo "Usage:"
+   echo " $(basename $0) [Options] [<Command> ...]"
+   echo "Options:"
+   echo " -h : Print usage information"
+   echo " -n : Dry-run print commands instead of executing them"
+   echo " -x : Enabling tracing of shell script"
+   echo " -q : Disable tracing of shell script"
+   echo
+}
+
+CMD="/bin/bash"
+while getopts 'hnxq' argv
+do
+    case $argv in
+    h)
+       usage
+       exit 0
+    ;;
+    n)
+       echo "Dry-Run"
+       DR=echo
+    ;;
+    q)
+        echo "Disable tracing"
+        set +x
+	CMD="/bin/bash"
+    ;;
+    x)
+        echo "Enabling tracing"
+        set -x
+	CMD="/bin/bash -x"
+    ;;
+    esac
+done
+# Obscure option syntax: A + to turn off a "no" option...
+set +o nounset
+if [ -n "$DR" ]; then
+    DO_CMD="cat"
+else
+    DO_CMD="$CMD"
+fi
+
+$DR cd "$(dirname "$0")"
 
 unset LC_CTYPE
 unset LANG
 
+$DO_CMD <<EOF
 mkdir -p "$(pwd)/build"
 export CARGO_HOME="$(pwd)/build/cargo"
 export RUSTUP_HOME="$(pwd)/build/rust"
@@ -22,3 +68,4 @@ rustup component add rust-src
 cargo install --locked --version 0.65.1 bindgen-cli
 rustup component add rustfmt
 rustup component add clippy
+EOF
